1247a3d3239a03085c1a957bf7e6a64197c41c90
lome-ezehi <lomeezehi@gmail.com>
2025-07-16 19:49:31 +0100

    Completed Home Page improvements

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,17 @@
 const yearSpan = document.querySelector('#currentYear');
 const currentYear = new Date();
 yearSpan.innerText = currentYear.getFullYear();
+
+// active links
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+        // e.preventDefault(); 
+
+        navLinks.forEach(l => l.classList.remove('active'));
+
+        this.classList.add('active');
+    });
+});
